ec37a6c257aaf0d4a68b79a3ac4c80dfcef3fba0
YoungPinch <dreamerandrew17@gmail.com>
2022-12-20 16:10:34 +0300

    up

--- traveling_salesman/traveling_salesman/main.cpp
+++ traveling_salesman/traveling_salesman/main.cpp
@@ -16,12 +16,14 @@ int main(int argc, char* argv[])
 {
   srand(3);
   setlocale(LC_ALL, "Russian");
-  AdjacencyMatrixG<int> ma(3);
+  AdjacencyMatrixG<int> ma(1000);
   ma.GenerateByRandom(RandomFunc);
-  AntColonyAlg alg(ma);
+  AntColonyAlg alg1(ma,2,0.5 ,0.2,1,5, 0.3, 1, 10);
+  std::cout << "Time = " << ts::TestSpeed::TestAlg(&alg1) << std::endl;
+  std::cout << "Result = " << alg1.GetMinWeight();
   BruteForceAlg bAlg(ma);
-  std::cout << "Матрица смежности имеет вид:" << std::endl;
-  std::cout << ma;
+  //std::cout << "Матрица смежности имеет вид:" << std::endl;
+  //std::cout << ma;
   std::cout << std::endl;
-  ts::TestSpeed::CompareAlgs(&alg, &bAlg);
+  //ts::TestSpeed::CompareAlgs(&alg1, &bAlg);
 }
